58f2c6a1abbdfb2958046ef53cae998e07453c39
basitol <41623944+basitol@users.noreply.github.com>
2022-05-28 02:20:04 +0100

    build: order model

---
+++ backend/models/orderModel.js
@@ -0,0 +1,80 @@
+import mongoose from "mongoose";
+
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    orderItems: [
+      {
+        name: { type: String, required: true },
+        qty: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: String, required: true },
+        Product: {
+          type: mongoose.Schema.Types.ObjectID,
+          required: true,
+          ref: product,
+        },
+      },
+    ],
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    paymentResult: {
+      id: { String },
+      status: { String },
+      update_time: { String },
+      email_address: { String },
+    },
+    taxPrice: {
+      type: String,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: String,
+      required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: String,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Date,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
