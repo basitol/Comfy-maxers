e15107b6446d73c8ae74201da01034a82db4590e
basitol <41623944+basitol@users.noreply.github.com>
2022-06-01 22:53:40 +0100

    updated export components

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -2,5 +2,7 @@ import Header from "./Header/Header";
 import Product from "./Product/Product";
 import Footer from "./Footer/Footer";
 import Rating from "./Rating/Rating";
+import Loader from "./Loader";
+import Message from "./Message";
 
-export { Header, Footer, Product, Rating };
+export { Header, Footer, Product, Rating, Loader, Message };
