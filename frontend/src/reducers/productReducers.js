e7e1e9eeeb055df8c0e61d2d260c8f3f55e40691
basitol <41623944+basitol@users.noreply.github.com>
2022-06-01 22:51:11 +0100

    updated reducer for products

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -2,6 +2,9 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAIL_REQUEST,
+  PRODUCT_DETAIL_SUCCESS,
+  PRODUCT_DETAIL_FAIL,
 } from "../constants/productConstants";
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -11,6 +14,22 @@ export const productListReducer = (state = { products: [] }, action) => {
     case PRODUCT_LIST_SUCCESS:
       return { loading: false, products: action.payload };
     case PRODUCT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const productDetailsReducer = (
+  state = { products: { reviews: [] } },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_DETAIL_REQUEST:
+      return { loading: true, ...state };
+    case PRODUCT_DETAIL_SUCCESS:
+      return { loading: false, products: action.payload };
+    case PRODUCT_DETAIL_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
