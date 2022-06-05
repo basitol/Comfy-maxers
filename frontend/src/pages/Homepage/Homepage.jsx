ff56a6b7e8ff61a78d9c07a4f822164bc33cc826
basitol <41623944+basitol@users.noreply.github.com>
2022-06-05 09:51:43 +0100

    update: Home page

--- frontend/src/pages/Homepage/Homepage.jsx
+++ frontend/src/pages/Homepage/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Row, Col } from "react-bootstrap";
 import { Product, Loader, Message } from "../../components";
