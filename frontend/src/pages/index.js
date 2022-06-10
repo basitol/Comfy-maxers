2b93daf20370386e168f44d00da3caead4416da6
basitol <41623944+basitol@users.noreply.github.com>
2022-06-10 09:26:30 +0100

    added login page to export

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,5 +1,6 @@
 import Homepage from "./Homepage/Homepage";
 import ProductPage from "./ProductPage/ProductPage";
 import CartPage from "./Cart/CartPage";
+import LoginPage from "./User/LoginPage";
 
-export { Homepage, ProductPage, CartPage };
+export { Homepage, ProductPage, CartPage, LoginPage };
