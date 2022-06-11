dee0198ccda087fa75870f971768fcd430c4feee
basitol <basitolaitan27@gmail.com>
2022-06-11 22:01:48 +0100

    Added: profile page to exports

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,5 +3,13 @@ import ProductPage from "./ProductPage/ProductPage";
 import CartPage from "./Cart/CartPage";
 import LoginPage from "./User/LoginPage";
 import RegisterPage from "./User/RegisterPage";
+import ProfilePage from "./User/ProfilePage";
 
-export { Homepage, ProductPage, CartPage, LoginPage, RegisterPage };
+export {
+  Homepage,
+  ProductPage,
+  CartPage,
+  LoginPage,
+  RegisterPage,
+  ProfilePage,
+};
