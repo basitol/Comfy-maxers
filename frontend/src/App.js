ec3927c9e059e21c65e4fae5956eba1c1f19d321
basitol <41623944+basitol@users.noreply.github.com>
2022-06-11 08:05:47 +0100

    Added register route to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,11 @@
 import { Header, Footer } from "./components";
-import { Homepage, ProductPage, CartPage, LoginPage } from "./pages";
+import {
+  Homepage,
+  ProductPage,
+  CartPage,
+  LoginPage,
+  RegisterPage,
+} from "./pages";
 import { Container } from "react-bootstrap";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
@@ -11,6 +17,7 @@ function App() {
           <Route index path="/" element={<Homepage />}></Route>
           <Route path="/product/:id" element={<ProductPage />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
           <Route path="/cart">
             <Route path=":id" element={<CartPage />} />
             <Route path="" element={<CartPage />} />
