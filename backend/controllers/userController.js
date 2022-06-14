fbddc7242a4690b01070a3504ef18de365365395
basitol <basitolaitan27@gmail.com>
2022-06-14 21:04:38 +0100

    removed: console message

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -18,6 +18,7 @@ const authUser = asyncHandler(async (req, res) => {
       isAdmin: user.isAdmin,
       token: generateToken(user._id),
     });
+    console.log(user);
   } else {
     res.status(401);
     throw new Error("Invalid email or password");
