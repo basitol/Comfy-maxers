9d03b5e62cfda40de587ced57a796770ae779cca
basitol <basitolaitan27@gmail.com>
2022-06-15 10:42:49 +0100

    Added: get order details to order actions

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -3,6 +3,9 @@ import {
   CREATE_ORDER_REQUEST,
   CREATE_ORDER_SUCCESS,
   CREATE_ORDER_FAIL,
+  ORDER_CREATED_REQUEST,
+  ORDER_CREATED_SUCCESS,
+  ORDER_CREATED_FAIL,
 } from "../constants/orderConstant";
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -31,6 +34,39 @@ export const createOrder = (order) => async (dispatch, getState) => {
   } catch (error) {
     dispatch({
       type: CREATE_ORDER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.response,
+    });
+  }
+};
+
+export const getOrderDetails = (id) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_CREATED_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/orders/${id}`, config);
+
+    dispatch({
+      type: ORDER_CREATED_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_CREATED_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
