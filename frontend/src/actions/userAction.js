07aa378f7982a8a04f19d61b3eca0be024572c53
basitol <basitolaitan27@gmail.com>
2022-06-17 10:48:35 +0100

    updated: user action

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -13,8 +13,10 @@ import {
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_FAIL,
-  USER_UPDATE_PROFILE_RESET,
+  USER_DETAILS_RESET,
 } from "../constants/userConstants";
+
+import { ORDER_LIST_RESET } from "../constants/orderConstant";
 
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -59,6 +61,12 @@ export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
   dispatch({
     type: USER_LOGOUT,
+  });
+  dispatch({
+    type: USER_DETAILS_RESET,
+  });
+  dispatch({
+    type: ORDER_LIST_RESET,
   });
 };
 
