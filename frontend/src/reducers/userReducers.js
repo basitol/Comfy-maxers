9082e476841c0b3f08bf9317b408a73d5350c10e
basitol <basitolaitan27@gmail.com>
2022-06-17 13:25:29 +0100

    build: users list reducer

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -14,6 +14,9 @@ import {
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_RESET,
   USER_DETAILS_RESET,
+  USER_LIST_FAIL,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -69,6 +72,19 @@ export const userUpdateProfileReducer = (state = {}, action) => {
       return { loading: false, errorMsg: action.payload };
     case USER_UPDATE_PROFILE_RESET:
       return {};
+    default:
+      return state;
+  }
+};
+
+export const userListReducer = (state = { users: [] }, action) => {
+  switch (action.type) {
+    case USER_LIST_REQUEST:
+      return { loading: true };
+    case USER_LIST_SUCCESS:
+      return { loading: false, users: action.payload };
+    case USER_LIST_FAIL:
+      return { loading: false, errorMsg: action.payload };
     default:
       return state;
   }
