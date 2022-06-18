708a6e9c10a6d5a6a38f09d0e1f7263666a0bc3b
basitol <basitolaitan27@gmail.com>
2022-06-18 23:13:14 +0100

    build: user list page

---
+++ frontend/src/pages/User/UserListPage.jsx
@@ -0,0 +1,80 @@
+import { useEffect } from "react";
+import { LinkContainer } from "react-router-bootstrap";
+import { Table, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { Message, Loader } from "../../components";
+import { listUsers } from "../../actions/userAction";
+
+import "./login.scss";
+import { FaCheck, FaEdit, FaTimes, FaTrash } from "react-icons/fa";
+const UserListPage = () => {
+  const dispatch = useDispatch();
+
+  const userList = useSelector((state) => state.userList);
+  const { loading, error, users } = userList;
+
+  useEffect(() => {
+    dispatch(listUsers());
+  }, [dispatch]);
+
+  const deleteHandler = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <h1>Users</h1>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message>{error}</Message>
+      ) : (
+        <Table striped bordered hover responsive className="table-sm">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>ADMIN</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id}>
+                <td>{user._id}</td>
+                <td>{user.name}</td>
+                <td>
+                  <a href={`mailto:${user.email}`}>{user.email}</a>
+                </td>
+                <td>
+                  {user.isAdmin ? (
+                    <FaCheck color="green" />
+                  ) : (
+                    <FaTimes color="red" />
+                  )}
+                </td>
+                <td>
+                  <LinkContainer to={`/user/${user._id}`}>
+                    <Button variant="outline-primary" className="btn-sm">
+                      <FaEdit />
+                    </Button>
+                  </LinkContainer>
+                  <Button
+                    variant="danger"
+                    className="btn-sm"
+                    onClick={() => deleteHandler(user._id)}
+                  >
+                    <FaTrash />
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
+    </>
+  );
+};
+
+export default UserListPage;
