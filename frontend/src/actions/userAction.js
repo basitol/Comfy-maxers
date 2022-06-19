ebf8a0dc6005902492f31fb21eb9374a6d6e089b
basitol <basitolaitan27@gmail.com>
2022-06-19 04:12:01 +0100

    Added: user list reset to user action

--- frontend/src/actions/userAction.js
+++ frontend/src/actions/userAction.js
@@ -17,6 +17,7 @@ import {
   USER_LIST_REQUEST,
   USER_LIST_SUCCESS,
   USER_LIST_FAIL,
+  USER_LIST_RESET,
 } from "../constants/userConstants";
 
 import { ORDER_LIST_RESET } from "../constants/orderConstant";
@@ -70,6 +71,9 @@ export const logout = () => (dispatch) => {
   });
   dispatch({
     type: ORDER_LIST_RESET,
+  });
+  dispatch({
+    type: USER_LIST_RESET,
   });
 };
 
