023ae08722a9ba86f947e8dd0bc5158b93c28b5a
basitol <basitolaitan27@gmail.com>
2022-06-19 09:01:47 +0100

    Add; delete functionality

--- frontend/src/pages/User/UserListPage.jsx
+++ frontend/src/pages/User/UserListPage.jsx
@@ -4,14 +4,14 @@ import { LinkContainer } from "react-router-bootstrap";
 import { Table, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Message, Loader } from "../../components";
-import { listUsers } from "../../actions/userAction";
+import { listUsers, deleteUser } from "../../actions/userAction";
 
 import "./login.scss";
 import { FaCheck, FaEdit, FaTimes, FaTrash } from "react-icons/fa";
 
 const UserListPage = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const dispatch = useDispatch(); // dispatch is the dispatch function from useDispatch
+  const navigate = useNavigate(); // navigate is a hook that we can use to navigate to other pages
 
   const userList = useSelector((state) => state.userList);
   const { loading, error, users } = userList;
@@ -19,16 +19,22 @@ const UserListPage = () => {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
+  const userDelete = useSelector((state) => state.userDelete);
+  const { success: successDelete } = userDelete;
+
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) {
-      dispatch(listUsers());
+      // if user is logged in and is an admin
+      dispatch(listUsers()); // get all users
     } else {
-      navigate("/login");
+      navigate("/login"); // if user is not logged in or is not an admin, navigate to login page
     }
-  }, [dispatch, userInfo, navigate]);
+  }, [dispatch, userInfo, navigate, successDelete]); // run this effect only when userInfo changes
 
-  const deleteHandler = (e) => {
-    e.preventDefault();
+  const deleteHandler = (id) => {
+    if (window.confirm("Are you sure you want to delete this user?")) {
+      dispatch(deleteUser(id));
+    }
   };
 
   return (
@@ -65,7 +71,7 @@ const UserListPage = () => {
                   )}
                 </td>
                 <td>
-                  <LinkContainer to={`/user/${user._id}`}>
+                  <LinkContainer to={`/admin/users/${user._id}`}>
                     <Button variant="outline-primary" className="btn-sm">
                       <FaEdit />
                     </Button>
