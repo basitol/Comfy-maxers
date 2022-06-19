4e093e0a174654bada6f1d13cb492fe43c2b5be5
basitol <basitolaitan27@gmail.com>
2022-06-19 08:55:50 +0100

    build: get user by id, update user by id controler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -129,6 +129,44 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 });
 
+// desc   Get user by id
+// route  GET /api/users/:id
+// access Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password"); // exclude password
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+// desc   Update user by id
+// route  PUT /api/users/:id
+// access Private/Admin
+const updateUserById = asyncHandler(async (req, res) => {
+  const user = await User.findByIdAndUpdate(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin || user.isAdmin;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
 export {
   authUser,
   registerUser,
@@ -136,4 +174,6 @@ export {
   updateUserProfile,
   getUsers,
   deleteUser,
+  getUserById,
+  updateUserById,
 };
