6b5c97ef635f16b91d8841e1b300b119211ebfb4
basitol <basitolaitan27@gmail.com>
2022-06-19 04:15:01 +0100

    build: delete user controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -114,4 +114,26 @@ const getUsers = asyncHandler(async (req, res) => {
   res.json(user);
 });
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers };
+// desc   Delete user
+// route  DELETE /api/users/:id
+// access Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    await user.remove();
+    res.status(200).json({ message: "User deleted" });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export {
+  authUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+};
