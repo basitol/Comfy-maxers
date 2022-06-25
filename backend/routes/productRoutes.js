f41f263b6048dc194b9220290b06327b0865bba0
basitol <basitolaitan27@gmail.com>
2022-06-25 13:20:11 +0100

    update route for update, delete and create product

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,10 +2,19 @@ import express from "express";
 import {
   getProductById,
   getProducts,
+  deleteProduct,
+  createProduct,
+  updateProduct,
 } from "../controllers/productController.js";
+import { protect, adminProtect } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 
-router.route("/").get(getProducts);
-router.route("/:id").get(getProductById);
+router.route("/").get(getProducts).post(protect, adminProtect, createProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, adminProtect, deleteProduct)
+  .put(protect, adminProtect, updateProduct);
 
 export default router;
