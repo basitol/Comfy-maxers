fa594106eaa8139ab6463366259e34da81f08feb
basitol <basitolaitan27@gmail.com>
2022-06-25 13:19:45 +0100

    update: deliver and get order routes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -5,13 +5,19 @@ import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  updateOrderToDelivered,
   getMyOrders,
+  getOrders,
 } from "../controllers/orderController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, adminProtect } from "../middleware/authMiddleware.js";
 
-router.route("/").post(protect, addOrderItems);
+router
+  .route("/")
+  .post(protect, addOrderItems)
+  .get(protect, adminProtect, getOrders);
 router.route("/myorders").get(protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/paid").put(protect, updateOrderToPaid);
+router.route("/:id/deliver").put(protect, adminProtect, updateOrderToDelivered);
 
 export default router;
