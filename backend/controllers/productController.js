dde121939e0d2e4d696fcf9a4f11c5241050f6fb
basitol <basitolaitan27@gmail.com>
2022-06-26 20:49:20 +0100

    build: review routes and pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,9 +5,23 @@ import Product from "../models/productModel.js";
 // route  GET /api/products
 // access Public
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
+  const pageSize = 10;
+  const page = Number(req.query.pageNumber) || 1;
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
 
-  res.json(products);
+  const count = await Product.count({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 // desc   Fetch single product
@@ -61,6 +75,48 @@ const createProduct = asyncHandler(async (req, res) => {
   res.status(201).json(createdProduct);
 });
 
+// desc   Create a new review
+// route  POST /api/product/id/reviews
+// access Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error("Product already reviewed");
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.numReviews = product.reviews.length;
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
+
+    await product.save();
+
+    res.status(200).json({ message: "Review added" });
+  } else {
+    res.status(404);
+    throw new Error("Product Not Found");
+  }
+});
+
 // desc   Update a product
 // route  POST /api/product/id
 // access Private/Admin
@@ -94,4 +150,5 @@ export {
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 };
