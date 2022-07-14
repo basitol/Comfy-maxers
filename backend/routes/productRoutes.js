bbccfc83427e2c64551fe56812055ecc7970f3bd
basitol <basitolaitan27@gmail.com>
2022-07-14 23:00:35 +0100

    updated new product route

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,6 +7,7 @@ import {
   updateProduct,
   createProductReview,
   getTopProduct,
+  getNewProduct,
 } from "../controllers/productController.js";
 import { protect, adminProtect } from "../middleware/authMiddleware.js";
 
@@ -15,6 +16,7 @@ const router = express.Router();
 router.route("/").get(getProducts).post(protect, adminProtect, createProduct);
 router.route("/:id/reviews").post(protect, createProductReview);
 router.get("/top", getTopProduct);
+router.get("/latest", getNewProduct);
 router
   .route("/:id")
   .get(getProductById)
